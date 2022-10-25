ea1e9a594945011dd202da7d3bded6e6e6f6c304
JerryWski <jarek_wasilewski@yahoo.com>
2022-10-26 01:06:27 +0200

    polish

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,20 @@
+const accordionQuestions = document.querySelectorAll(".accordion__box");
 
-const accordionQuestions = document.querySelectorAll('.accordion__box');
+accordionQuestions.forEach((question) => {
+  question.addEventListener("click", () => {
+    console.log(question);
+    if (question.classList.contains("active")) {
+      question.classList.remove("active");
+    } else {
+      closingAccordeon();
+      question.classList.add("active");
+    }
+  });
+});
 
-accordionQuestions.forEach(question => {
-    question.addEventListener('click', () => {
-        console.log(question);
-        if(question.classList.contains('active')) {
-            question.classList.remove('active')
-        }else {
-            const activeQuestion = document.querySelectorAll(".active");
-            activeQuestion.forEach(questionActivated => {
-                questionActivated.classList.remove('active')
-            })
-            question.classList.add('active')
-        }
-    })
-})
+const closingAccordeon = () => {
+  const activeQuestion = document.querySelectorAll(".active");
+  activeQuestion.forEach((questionActivated) => {
+    questionActivated.classList.remove("active");
+  });
+};
